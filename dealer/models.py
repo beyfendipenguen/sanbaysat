6b855e6cb3c8e198648e40d00f8e80f6729222dc
beyfendipenguen <beyfendipenguen@gmail.com>
2021-10-31 20:43:04 +0300

    update dealer models

--- dealer/models.py
+++ dealer/models.py
@@ -1,4 +1,23 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from accounts.models import UserAccount
 # Create your models here.
 
+
+class Dealer(models.Model):
+    dealer_user = models.OneToOneField(
+        UserAccount, on_delete=models.DO_NOTHING)
+    dealer_name = models.CharField(max_length=30, unique=True)
+    dealer_address = models.CharField(max_length=150)
+    dealer_city = models.CharField(max_length=30)
+    dealer_country = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = _("Dealer")
+        verbose_name_plural = _("dealers")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Dealer_details", kwargs={"pk": self.pk})
