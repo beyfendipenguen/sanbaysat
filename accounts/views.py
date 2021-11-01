e701256bd83f3ee8cd695d2b644f97f5977bdf8c
beyfendipenguen <beyfendipenguen@gmail.com>
2021-11-01 23:14:35 +0300

    add CreateView for sign up

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from accounts.models import UserAccount
 # Create your views here.
+
+
+class AccountCreateView(CreateView):
+    model = UserAccount
+    template_name = "signup.html"
+    fields = ['email', 'first_name', 'last_name', 'password', 'date_of_birth']
